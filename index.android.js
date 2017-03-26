2ed16791ca5f83191da58ddc79b56086ba129a42
forzaster <forzaster@gmail.com>
2017-03-26 12:31:54 +0900

    [update] Added styles

--- index.android.js
+++ index.android.js
@@ -1,15 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet } from 'react-native';
+import { AppRegistry } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './app/components/App';
 import reducer from './app/reducers';
+import styles from './app/styles'
 
 const store = createStore(reducer);
 
@@ -22,24 +17,5 @@ export default class SimpleMemo extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('SimpleMemo', () => SimpleMemo);
