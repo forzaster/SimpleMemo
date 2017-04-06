95712eafc2a4cd38de9616c1c9b27b24d4b05839
forzaster <forzaster@gmail.com>
2017-04-07 00:01:02 +0900

    [fix] Fixed actionShowAddMemo

--- app/actions/index.js
+++ app/actions/index.js
@@ -14,9 +14,8 @@ export const ACTION_SHOW_ADD_MEMO = 'SHOW_ADD_MEMO'
 export const ACTION_ADD_MEMO = 'ADD_MEMO'
 export const ACTION_UPDATE_MEMO = 'UPDATE_MEMO'
 
-export const actionShowAddMemo = (d) => ({
-  type: ACTION_SHOW_ADD_MEMO,
-  data: d
+export const actionShowAddMemo = () => ({
+  type: ACTION_SHOW_ADD_MEMO
 })
 export const actionAddMemo = (d) => ({
   type: ACTION_ADD_MEMO,
